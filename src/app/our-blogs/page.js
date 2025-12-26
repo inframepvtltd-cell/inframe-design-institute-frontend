7ff248fe4b6be21f75b0c8b22f0a8b6d6a339ff2
inframepvtltd-cell <tarunmehra80790@gmail.com>
2025-12-26 17:23:20 +0530

    new commit

--- src/app/our-blogs/page.js
+++ src/app/our-blogs/page.js
@@ -7,7 +7,6 @@ export default function Blogs() {
 
     const [activeTab, setActiveTab] = useState('All')
     const Categories = [
-        "All",
         "Education",
         "Career",
         "Facilities",
@@ -37,40 +36,48 @@ export default function Blogs() {
 
     return (
         <div className="w-full ">
-            <h3 className="px-6 lg:text-[40px] text-center text-gray-900 text-[25px] font-semibold   pt-5  capitalize">Our Blogs</h3>
-            <div className="flex flex-col p-3 bg-white  mb-5 items-center gap-5">
-                <p className="text-center lg:text-[30px] text-[20px] mb-2 text-gray-900">Discover insights, stories, and the latest updates from Inframe School</p>
-                <div className="relative bg-white w-full max-w-md rounded-md">
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        {/* Search Icon (SVG) */}
-                        <IoMdSearch
-                            className="text-[25px] text-gray-900"
+            <div className="max-w-[1320] mx-auto">
+                <h3 className="lg:text-[40px] text-gray-900 text-[25px] font-bold   pt-5  capitalize">Our Blogs</h3>
+                <div className="flex flex-col bg-white  gap-5">
+                    <p className="text-[20px] mb-2 text-gray-900">Discover insights, stories, and the latest updates from Inframe School</p>
+                    <div className="relative bg-white w-full max-w-md rounded-md">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            {/* Search Icon (SVG) */}
+                            <IoMdSearch
+                                className="text-[25px] text-gray-900"
+                            />
+                        </span>
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full border-[1] border-gray-900 pl-10 text-md pr-4 py-3 rounded-md focus:outline-none focus:ring-[2px] focus:border-transparent"
                         />
-                    </span>
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="w-full border-[1] border-gray-900 pl-10 text-md pr-4 py-3 rounded-md focus:outline-none focus:ring-[2px] focus:border-transparent"
-                    />
+                    </div>
                 </div>
             </div>
 
-            <div className="max-w-[1320] mx-auto lg:px-6 px-3 " >
-                <span className="text-[30px] border-b-[3] text-gray-900">Categories </span>
+            <div className="max-w-[1320] mx-auto lg:px-0 px-3 " >
+                <h1 className="text-[40px] mt-5 font-bold text-gray-900">Categories </h1>
+                <p className="text-[20px] mb-2 text-gray-900">Discover insights, stories, and the latest updates from Inframe School</p>
                 <div className="w-full scrollbar overflow-x-auto my-5">
-                    <div className="flex whitespace-nowrap">
-                        {Categories.map((item, index) => {
-                            return (
+                    <div className="overflow-hidden w-full">
+                        <div className="flex whitespace-nowrap animate-scroll">
+                            {[...Categories, ...Categories].map((item, index) => (
                                 <button
-                                    onClick={() => setActiveTab(item)}
                                     key={index}
-                                    className={`${item == activeTab ? 'bg-gray-900 text-white border-gray-900' : 'bg-transparent text-gray-900 border-gray-900'} mb-4 mx-[3px]  border-[1]   px-4 py-2 rounded-[5px] cursor-pointer hover:bg-gray-900 hover:border-transparent hover:text-white duration-300 `}
+                                    onClick={() => setActiveTab(item)}
+                                    className={`${item === activeTab
+                                        ? 'bg-gray-900 text-white border-gray-900'
+                                        : 'bg-transparent text-gray-900 border-gray-900'
+                                        } mx-[3px] border px-4 py-2 rounded-[5px] cursor-pointer hover:bg-gray-900 hover:border-transparent hover:text-white duration-300`}
                                 >
                                     {item}
                                 </button>
-                            );
-                        })}
+                            ))}
+                        </div>
                     </div>
+
+
                 </div>
                 <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8 my-[40px]">
                     {categoryData.map((item, index) => {
